e0bb4901c8ff6fd5a7136a3dfaf0a2d5aeba8f44
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-02-06 16:17:02 -0300

    Verificação

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form-agenda')
 let lines =''
-let formisValid = false
+let formIsValid = false
+const contacts = []
+const telNumbers = []
 
 function nameValidation(nameContact) {
     const nameArray = nameContact.split(' ')
@@ -10,13 +12,37 @@ function nameValidation(nameContact) {
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
+    addLines()
+    updateLines()
+})
+
+function addLines() {
+
     const nameContact = document.getElementById('name-contact')
     const telContact = document.getElementById('tel-contact')
+    const errorM = "Por favor preencha com Nome e Sobrenome"
+    const warningM = `O numero ${telContact} já esta na sua agenda!`
+    
+    formIsValid = nameValidation(nameContact.value)
 
-    const warningMessage = "Por favor preencha com Nome e Sobrenome"
-
-    formisValid = nameValidation(nameContact.value)
-    if (formisValid) {
+    if (formIsValid === false) {
+        const errorMessage = document.querySelector('.error')
+        errorMessage.innerHTML = errorM
+        errorMessage.style.display = 'block'
+        
+        setTimeout(function() {
+            errorMessage.style.display = 'none'
+        }, 5000)
+        
+    } else if (telNumbers.includes(parseFloat(telContact.value))){
+        const warningMessage = document.querySelector('.warning')
+        warningMessage.innerHTML = warningM
+        warningMessage.style.display = 'block'
+        
+        setTimeout(function() {
+            warningMessage.style.display = 'none'
+        }, 5000)
+    } else {
 
         let line = '<tr>'
         line += `<td>${nameContact.value}</td>`
@@ -24,17 +50,10 @@ form.addEventListener('submit', function(e) {
         line += '</tr>'
 
         lines += line
+    }
+}
+function updateLines () {
 
         const tableBody = document.querySelector('tbody')
         tableBody.innerHTML = lines
-    } else {
-        const errorMessage = document.querySelector('.warning')
-        errorMessage.innerHTML = warningMessage
-        errorMessage.style.display = 'block'
-        
-        setTimeout(function() {
-            errorMessage.style.display = 'none'
-        }, 5000)
-    }
-    
-})
+}
